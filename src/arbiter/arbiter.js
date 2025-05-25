5a42bbdbc9087a02f6733fe2272b7fb78ba9a890
utkxrsh17 <utgp17@gmail.com>
2025-05-26 02:49:23 +0530

    added the moves of all pieces

--- src/arbiter/arbiter.js
+++ src/arbiter/arbiter.js
@@ -1,7 +1,35 @@
-import { getRookMoves } from "./getMoves";
+import {
+  getRookMoves,
+  getKnightMoves,
+  getBishopMoves,
+  getQueenMoves,
+  getKingMoves,
+  getPawnCaptures,
+  getPawnMoves,
+} from "./getMoves";
 const arbiter = {
   getRegularMoves: function ({ position, piece, rank, file }) {
-    return getRookMoves({ position, piece, rank, file });
+    if (piece.endsWith("n")) {
+      return getKnightMoves({ position, piece, rank, file });
+    }
+    if (piece.endsWith("r")) {
+      return getRookMoves({ position, piece, rank, file });
+    }
+    if (piece.endsWith("b")) {
+      return getBishopMoves({ position, piece, rank, file });
+    }
+    if (piece.endsWith("q")) {
+      return getQueenMoves({ position, piece, rank, file });
+    }
+    if (piece.endsWith("k")) {
+      return getKingMoves({ position, piece, rank, file });
+    }
+    if (piece.endsWith("p")) {
+      return [
+        ...getPawnMoves({ position, piece, rank, file }),
+        ...getPawnCaptures({ position, piece, rank, file }),
+      ];
+    }
   },
 };
 
--- src/arbiter/getMoves.js
+++ src/arbiter/getMoves.js
@@ -28,3 +28,141 @@ export const getRookMoves = ({ position, piece, rank, file }) => {
   });
   return moves;
 };
+export const getKnightMoves = ({ position, piece, rank, file }) => {
+  const moves = [];
+  const us = piece[0];
+  const enemy = position[rank][file].startsWith("w") ? "b" : "w";
+
+  const candidates = [
+    [-2, -1],
+    [-2, 1],
+    [2, -1],
+    [2, 1],
+    [1, 2],
+    [1, -2],
+    [-1, 2],
+    [-1, -2],
+  ];
+
+  candidates.forEach((c) => {
+    const newRank = rank + c[0];
+    const newFile = file + c[1];
+    if (newRank >= 0 && newRank < 8 && newFile >= 0 && newFile < 8) {
+      const cell = position[newRank][newFile];
+      if (cell !== undefined && (cell.startsWith(enemy) || cell === "")) {
+        moves.push([newRank, newFile]);
+      }
+    }
+  });
+  return moves;
+};
+export const getBishopMoves = ({ position, piece, rank, file }) => {
+  const moves = [];
+  const us = piece[0];
+  const enemy = position[rank][file].startsWith("w") ? "b" : "w";
+
+  const direction = [
+    [1, 1],
+    [1, -1],
+    [-1, 1],
+    [-1, -1],
+  ];
+
+  direction.forEach((dir) => {
+    for (let i = 1; i < 8; i++) {
+      const x = rank + i * dir[0];
+      const y = file + i * dir[1];
+
+      if (position?.[x]?.[y] === undefined) break;
+      if (position[x][y].startsWith(enemy)) {
+        moves.push([x, y]);
+        break;
+      }
+      if (position[x][y].startsWith(us)) {
+        break;
+      }
+      moves.push([x, y]);
+    }
+  });
+  return moves;
+};
+export const getQueenMoves = ({ position, piece, rank, file }) => {
+  const moves = [
+    ...getRookMoves({ position, piece, rank, file }),
+    ...getBishopMoves({ position, piece, rank, file }),
+  ];
+
+  return moves;
+};
+export const getKingMoves = ({ position, piece, rank, file }) => {
+  const moves = [];
+  const enemy = position[rank][file].startsWith("w") ? "b" : "w";
+  const us = piece[0];
+
+  const candidates = [
+    [0, 1],
+    [0, 1],
+    [1, 0],
+    [-1, 0],
+    [1, 1],
+    [1, -1],
+    [-1, 1],
+    [-1, -1],
+  ];
+
+  candidates.forEach((c) => {
+    const newRank = rank + c[0];
+    const newFile = file + c[1];
+    if (newRank >= 0 && newRank < 8 && newFile >= 0 && newFile < 8) {
+      const cell = position[newRank][newFile];
+      if (cell !== undefined && (cell.startsWith(enemy) || cell === "")) {
+        moves.push([newRank, newFile]);
+      }
+    }
+  });
+  return moves;
+};
+export const getPawnMoves = ({ position, piece, rank, file }) => {
+  const moves = [];
+  const dir = piece === "wp" ? 1 : -1;
+
+  // Move two tiles on first move
+  if (rank % 5 === 1) {
+    if (
+      position?.[rank + dir]?.[file] === "" &&
+      position?.[rank + dir + dir]?.[file] === ""
+    ) {
+      moves.push([rank + dir + dir, file]);
+    }
+  }
+
+  // Move one tile
+  if (!position?.[rank + dir]?.[file]) {
+    moves.push([rank + dir, file]);
+  }
+
+  return moves;
+};
+export const getPawnCaptures = ({ position, piece, rank, file }) => {
+  const moves = [];
+  const dir = piece === "wp" ? 1 : -1;
+  const enemy = piece[0] === "w" ? "b" : "w";
+
+  // Capture enemy to left
+  if (
+    position?.[rank + dir]?.[file - 1] &&
+    position[rank + dir][file - 1].startsWith(enemy)
+  ) {
+    moves.push([rank + dir, file - 1]);
+  }
+
+  // Capture enemy to right
+  if (
+    position?.[rank + dir]?.[file + 1] &&
+    position[rank + dir][file + 1].startsWith(enemy)
+  ) {
+    moves.push([rank + dir, file + 1]);
+  }
+
+  return moves;
+};
